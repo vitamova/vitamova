0087983fe6baca35774315c9decee2711791e959
Wesley Belleman <bellemanwesley@gmail.com>
2026-02-22 18:52:41 -0500

    adding first pages

--- webapp/webapp/settings.py
+++ webapp/webapp/settings.py
@@ -37,6 +37,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # allauth apps
+    'django.contrib.sites',
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
+    'allauth.socialaccount.providers.google'
 ]
 
 MIDDLEWARE = [
