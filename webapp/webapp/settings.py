3481a29f7d20419e6841aef020aa02c0775e1c9a
Wesley Belleman <bellemanwesley@gmail.com>
2026-02-22 18:54:32 -0500

    adding first pages

--- webapp/webapp/settings.py
+++ webapp/webapp/settings.py
@@ -46,12 +46,20 @@ INSTALLED_APPS = [
     'allauth.socialaccount.providers.google'
 ]
 
+SITE_ID = 1
+
+AUTHENTICATION_BACKENDS = [
+    'django.contrib.auth.backends.ModelBackend',
+    'allauth.account.auth_backends.AuthenticationBackend',
+]
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'allauth.account.middleware.AccountMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
