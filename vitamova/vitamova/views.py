644a4e2afdbbc4f0130d16a98da4f0f595b53aff
Wesley Belleman <bellemanwesley@gmail.com>
2026-04-25 17:43:16 -0400

    testing editing ability on mac

--- vitamova/vitamova/views.py
+++ vitamova/vitamova/views.py
@@ -11,7 +11,7 @@ import re
 import json
 from openai import OpenAI
 
-
+#Seeing if I can edit
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
