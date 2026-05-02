71d6aba5c970584103086b5e137ef3b4a2b22d74
Wesley Belleman <bellemanwesley@gmail.com>
2026-05-02 17:11:14 -0400

    adding ability to change score

--- vitalib/db.py
+++ vitalib/db.py
@@ -58,14 +58,14 @@ class UserInfo:
             self.username = username
         def score(self, new_score):
             with self.conn.cursor() as cur:
-                cur.execute("UPDATE users SET vocab_score=%s WHERE username=%s", (new_score, self.username))
+                cur.execute("UPDATE registered_user SET vocab_score=%s WHERE username=%s", (new_score, self.username))
     class Get:
         def __init__(self, conn, username):
             self.conn = conn
             self.username = username
         def score(self):
             with self.conn.cursor() as cur:
-                cur.execute("SELECT vocab_score FROM users WHERE username=%s", (self.username,))
+                cur.execute("SELECT vocab_score FROM registered_user WHERE username=%s", (self.username,))
                 return cur.fetchone()[0]
 
 class vocabulary:
