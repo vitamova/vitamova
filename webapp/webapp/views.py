076faf208a349999fdedeef458e51c417452ceca
Wesley Belleman <bellemanwesley@gmail.com>
2026-05-07 21:11:13 -0400

    finishing touches

--- webapp/webapp/views.py
+++ webapp/webapp/views.py
@@ -229,10 +229,7 @@ def create_customer_portal_session(request):
 
     portal_session = stripe.billing_portal.Session.create(
         customer=stripe_customer_id,
-        return_url=request.build_absolute_uri("/"),
-        branding_settings={
-            "display_name": "Vitamova"
-        },
+        return_url=request.build_absolute_uri("/")
     )
 
     return redirect(portal_session.url)
