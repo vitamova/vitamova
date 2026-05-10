0fff5ba742c0a2281e8138625c49b24aa360a6ab
Wesley Belleman <bellemanwesley@gmail.com>
2026-05-10 19:49:06 -0400

    fixing bug in multi-language capability

--- webapp/webapp/views.py
+++ webapp/webapp/views.py
@@ -881,15 +881,7 @@ def vocab_test(request):
                 score = base_score + bonus
                 score = max(0, min(6000, score))
 
-                with connection.cursor() as cursor:
-                    cursor.execute(
-                        """
-                        UPDATE registered_user
-                        SET vocab_score = %s
-                        WHERE user_id = %s
-                        """,
-                        [score, request.user.id]
-                    )
+                vitalib.UserInfo.Update(connection, request.user.id).score(language, score)
 
                 return JsonResponse({
                     "status": "complete",
