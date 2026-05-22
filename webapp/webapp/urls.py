0d16844127436e412ce4a63e0e95cac382b16681
Wesley Belleman <bellemanwesley@gmail.com>
2026-05-22 05:56:34 -0400

    reorganizing and adding gtag

--- webapp/webapp/urls.py
+++ webapp/webapp/urls.py
@@ -34,6 +34,7 @@ urlpatterns = [
 
     # New organized sections
     path("vocabulary/", include("webapp.new_urls.vocabulary")),
+    path("subscribe/", include("webapp.new_urls.subscribe")),
     #path("sentences/", include("webapp.urls.sentences")),
     #path("reading/", include("webapp.urls.reading"))
     ]
