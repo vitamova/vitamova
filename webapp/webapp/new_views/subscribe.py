ad1831ba8e72a6bc309c36c4ff227472ee5b9767
Wesley Belleman <bellemanwesley@gmail.com>
2026-05-22 07:09:42 -0400

    free 30 day trial

--- webapp/webapp/new_views/subscribe.py
+++ webapp/webapp/new_views/subscribe.py
@@ -64,7 +64,8 @@ def create_checkout_session(request):
                     "user_id": str(request.user.id),
                     "product": "vitamova",
                     "price_id": price_id,
-                }
+                },
+                "trial_period_days": 30  # 30 day free trial
             },
         )
 
