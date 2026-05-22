ca953343a29124372033a5a80ac5d331f7315489
Wesley Belleman <bellemanwesley@gmail.com>
2026-05-22 06:05:43 -0400

    adding subscribe success

--- vitalib/transform.py
+++ vitalib/transform.py
@@ -1,28 +1,29 @@
-class language:
-    def __init__ (self, language):
-        self.language = language
-    def code_to_name(self):
-        if self.language == "en":
-            return "English"
-        elif self.language == "es":
-            return "Spanish"
-        elif self.language == "fr":
-            return "French"
-        elif self.language == "de":
-            return "German"
-        elif self.language == "it":
-            return "Italian"
-        elif self.language == "ja":
-            return "Japanese"
-        elif self.language == "ko":
-            return "Korean"
-        elif self.language == "pt":
-            return "Portuguese"
-        elif self.language == "ru":
-            return "Russian"
-        elif self.language == "zh":
-            return "Chinese"
-        elif self.language == "uk":
-            return "Ukrainian"
-        else:
-            return "Unknown"
+class Transform:
+    class Language:
+        def __init__ (self, language):
+            self.language = language
+        def code_to_name(self):
+            if self.language == "en":
+                return "English"
+            elif self.language == "es":
+                return "Spanish"
+            elif self.language == "fr":
+                return "French"
+            elif self.language == "de":
+                return "German"
+            elif self.language == "it":
+                return "Italian"
+            elif self.language == "ja":
+                return "Japanese"
+            elif self.language == "ko":
+                return "Korean"
+            elif self.language == "pt":
+                return "Portuguese"
+            elif self.language == "ru":
+                return "Russian"
+            elif self.language == "zh":
+                return "Chinese"
+            elif self.language == "uk":
+                return "Ukrainian"
+            else:
+                return "Unknown"
--- webapp/webapp/new_views/subscribe.py
+++ webapp/webapp/new_views/subscribe.py
@@ -1,8 +1,13 @@
 from webapp.decorators import registered_logged_in_required
 from django.shortcuts import render
+from django.db import connection
+import vitalib
 
 @registered_logged_in_required
 def success(request):
+    user_target_language = vitalib.Database.UserInfo.Get(connection, request.user.id).data("target_language")["target_language"]
+    language_name = vitalib.Transform.Language(user_target_language).code_to_name()
     return render(request, "general/subscribe_success.html", {
-        "first_name": request.user.first_name
+        "first_name": request.user.first_name,
+        "language": language_name,
     })
