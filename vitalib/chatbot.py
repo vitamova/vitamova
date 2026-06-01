b2efed0db4d95d97154ca207cd09a6b1fb177183
Wesley Belleman <bellemanwesley@gmail.com>
2026-06-01 08:33:36 -0400

    updated chatbot

--- vitalib/chatbot.py
+++ vitalib/chatbot.py
@@ -3,29 +3,30 @@ from openai import OpenAI
 from google import genai
 
 
-class OpenAIChatbot:
-    def __init__(self, model):
-        self.model = model
-        self.client = OpenAI(api_key=os.getenv("OPENAI_KEY"))
+class Chatbot:
+    class OpenAI:
+        def __init__(self, model):
+            self.model = model
+            self.client = OpenAI(api_key=os.getenv("OPENAI_KEY"))
 
-    def send_message(self, message):
-        response = self.client.chat.completions.create(
-            model=self.model,
-            messages=[
-                {"role": "user", "content": message}
-            ],
-        )
-        return response.choices[0].message.content
+        def send_message(self, message):
+            response = self.client.chat.completions.create(
+                model=self.model,
+                messages=[
+                    {"role": "user", "content": message}
+                ],
+            )
+            return response.choices[0].message.content
 
 
-class GeminiChatbot:
-    def __init__(self, model):
-        self.model = model
-        self.client = genai.Client(api_key=os.getenv("GEMINI_KEY"))
+    class Gemini:
+        def __init__(self, model):
+            self.model = model
+            self.client = genai.Client(api_key=os.getenv("GEMINI_KEY"))
 
-    def send_message(self, message):
-        response = self.client.models.generate_content(
-            model=self.model,
-            contents=message,
-        )
-        return response.text
+        def send_message(self, message):
+            response = self.client.models.generate_content(
+                model=self.model,
+                contents=message,
+            )
+            return response.text
