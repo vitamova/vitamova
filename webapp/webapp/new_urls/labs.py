6c56d1e7c2af1511c38a16fbf2943fb78356ecc5
Wesley Belleman <bellemanwesley@gmail.com>
2026-06-16 16:04:19 -0400

    this should work

--- webapp/webapp/new_urls/labs.py
+++ webapp/webapp/new_urls/labs.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from webapp.new_views.labs import (
-    writing
+    writing,
+    speaking
 )
 
 urlpatterns = [
-    path("writing/", writing, name="writing_lab")
+    path("writing/", writing, name="writing_lab"),
+    path("speaking/", speaking, name="speaking_lab")
 ]
